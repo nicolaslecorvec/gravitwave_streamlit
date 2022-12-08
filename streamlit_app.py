2357a7e27881d355b01546561aba17b9e273ed3a
nicolas le corvec <nicolaslecorvec@gmail.com>
2022-12-08 14:43:26 +0100

    api working

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,37 +1,42 @@
-import scipy
-import streamlit as st
-#import os
-import pandas as pd
-import matplotlib.pyplot as plt
+import json
+import time
+from utils.wave import mywave
 import h5py
+import matplotlib.pyplot as plt
+import requests
+import streamlit as st
 
 from extract_hdf5 import extract_data_from_hdf5
-from spectrogram import plot_spectrograms
-from wave import mywave
+from utils.spectrogram import plot_spectrograms
 
 st.markdown("# Looking for Continuous Gravitational Waves?")
 
 st.markdown("## Select your new recording")
 uploaded_file = st.file_uploader("Pick a HDF5 file!", type='hdf5', key="recording1")
+url = "https://gravitwave-53od7qupsq-ew.a.run.app/predict_image"
+#url = 'http://localhost:8000/predict_image'
 
 if uploaded_file is not None:
     # We generate byte data from the uploaded sample 1 to store locally
-    data = extract_data_from_hdf5(uploaded_file)
-    import time
+    files = {"hdf5_file":uploaded_file}
 
-    # Add a placeholder
-    latest_iteration = st.empty()
-    bar = st.progress(0)
+    try:
+        r = requests.post(url, files=files)
+        r.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        raise SystemExit(err)
 
-    for i in range(100):
-        # Update the progress bar with each iteration.
-        latest_iteration.text(f"The AI analyzes your file... {i+1}")
-        bar.progress(i + 1)
-        time.sleep(0.07)
+    result = r.json()
 
-    st.markdown("## % chance that your data is true")
-    st.markdown("67,8%")
+    if result["prediction"] > 0.60:
+        st.success(f'This is a pretty good news ! You have a score of {result["prediction"]}', icon="✅")
+    else :
+        st.error(f'Bad news ! Try an another file, your score is only {result["prediction"]}', icon="🚨")
 
+    #st.write(result["prediction"])
+
+
+    data = extract_data_from_hdf5(uploaded_file.name)
 
     st.markdown("## Visual analysis of your data:")
     st.markdown("### Spectrogram visualization")
---
+++ utils/wave.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import streamlit as st
+from scipy.signal import istft
+from scipy import signal
+
+
+def mywave(data_s):
+
+    amp1 = data_s['L1_SFTs_amplitudes']
+    f1 = data_s['freq']
+    f_m1 = sum(f1)/len(f1)
+    st.write('Mean of label 1 signal frequency is {}'.format(f_m1))
+    _, xrec1 = signal.istft(amp1, f_m1)
+    xrec1_r = signal.resample(xrec1, 16707)
+
+    fig = plt.figure(figsize=(20,10))
+
+    plt.subplot(1, 2, 1)
+    plt.suptitle('Signals in Time Domain', fontsize=10)
+    plt.ylabel('Signal', fontsize=16)
+    plt.xlabel('Time', fontsize=16)
+
+    plt.plot(xrec1_r, label='Label 1 signal')
+    plt.legend(loc='upper right')
+    #face = misc.face(gray=True)
+    #ax.imshow(face, cmap='gray')
+    st.pyplot(fig)
