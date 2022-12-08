7aa927c69ccf1dcba8588d68e22deabf916d8f55
nicolas le corvec <nicolaslecorvec@gmail.com>
2022-12-08 15:39:38 +0100

    api working

--- utils/wave.py
+++ utils/wave.py
@@ -6,12 +6,16 @@ from scipy import signal
 
 def mywave(data_s):
 
-    amp1 = data_s['L1_SFTs_amplitudes']
-    f1 = data_s['freq']
+    amp1 = data_s['L1_SFTs_amplitudes'][0:500]
+    amp2 = data_s['H1_SFTs_amplitudes'][0:500]
+    f1 = data_s['freq'][0:500]
     f_m1 = sum(f1)/len(f1)
-    st.write('Mean of label 1 signal frequency is {}'.format(f_m1))
+    #st.write('Mean of label 1 signal frequency is {}'.format(f_m1))
     _, xrec1 = signal.istft(amp1, f_m1)
     xrec1_r = signal.resample(xrec1, 16707)
+
+    _, xrec2 = signal.istft(amp2, f_m1)
+    xrec2_r = signal.resample(xrec2, 16707)
 
     fig = plt.figure(figsize=(20,10))
 
@@ -20,7 +24,8 @@ def mywave(data_s):
     plt.ylabel('Signal', fontsize=16)
     plt.xlabel('Time', fontsize=16)
 
-    plt.plot(xrec1_r, label='Label 1 signal')
+    plt.plot(xrec1_r, label='Label L1 signal')
+    plt.plot(xrec2_r, label='Label H1 signal')
     plt.legend(loc='upper right')
     #face = misc.face(gray=True)
     #ax.imshow(face, cmap='gray')
